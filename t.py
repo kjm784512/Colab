552fb07e97f79c672e9926373899dbda154053e5
김지민 <125881468+kjm784512@users.noreply.github.com>
2023-10-17 10:40:18 +0900

    Create t.py

---
+++ t.py
@@ -0,0 +1,50 @@
+import turtle
+from turtle import *
+t = turtle.Turtle()
+
+t1 = turtle.Turtle()
+t1.speed(0)
+colormode(255)
+
+
+t1 = turtle.Turtle()
+t1.speed(0)
+dist = 1
+step = 1
+angle = 70
+red = 255
+green = 0
+blue = 0
+while True:
+    t1.color(red, green, blue)
+    t1.right(angle)
+    t1.forward(dist)
+    dist += step
+    red = red + 30
+    #green이 증가하는 경우
+    if red > 255:
+        red = 0
+        green = green + 30
+    #blue가 증가하는 이유
+    if green >= 255:
+        green = 0
+        blue = blue + 30
+
+    if blue >=255:
+        blue = 0
+turtle.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
